8f16aede87d8b69abd9699d2066f1c1c88a12233
GottaCodeHarder <masterkonnen@gmail.com>
2018-12-10 12:37:05 +0100

    Merged Wwise Includes into FirulWwise
    For export simplification purposes

--- Tabula Rasa Engine/3D_Engine/FirulWwise.h
+++ Tabula Rasa Engine/3D_Engine/FirulWwise.h
@@ -1,6 +1,39 @@
 #ifndef __FIRULWWISE_H__
 #define __FIRULWWISE_H__
 
+#include <AK/SoundEngine/Common/AkSoundEngine.h>
+#include <AK/MusicEngine/Common/AkMusicEngine.h>
+#include <AK/SoundEngine/Common/AkMemoryMgr.h>
+#include <AK/SoundEngine/Common/AkModule.h>
+#include <AK/SoundEngine/Common/AkStreamMgrModule.h>
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+// FirulWwise
+#include <AK/SoundEngine/Common/IAkStreamMgr.h>
+#include <AK/Tools/Common/AkPlatformFuncs.h>
+#include "AK/samples/Win32/AkFilePackageLowLevelIOBlocking.h"
+
+// Input libraries
+#ifndef _DEBUG  // Profile build configuration must be loaded instead of Debug
+#define AK_OPTIMIZED
+#pragma comment( lib, "AK/libraries/Release(StaticCRT)/lib/AkSoundEngine.lib")
+#pragma comment( lib, "AK/libraries/Release(StaticCRT)/lib/AkMusicEngine.lib")
+#pragma comment( lib, "AK/libraries/Release(StaticCRT)/lib/AkMemoryMgr.lib")
+#pragma comment( lib, "AK/libraries/Release(StaticCRT)/lib/AkStreamMgr.lib")
+
+#else
+#include <AK/Comm/AkCommunication.h>
+#pragma comment( lib, "AK/libraries/Debug(StaticCRT)/lib/CommunicationCentral.lib" )
+#pragma comment( lib, "AK/libraries/Debug(StaticCRT)/lib/AkSoundEngine.lib" )
+#pragma comment( lib, "AK/libraries/Debug(StaticCRT)/lib/AkMusicEngine.lib" )
+#pragma comment( lib, "AK/libraries/Debug(StaticCRT)/lib/AkMemoryMgr.lib" )
+#pragma comment( lib, "AK/libraries/Debug(StaticCRT)/lib/AkStreamMgr.lib" )
+
+#endif
+
+#pragma comment( lib, "AK/libraries/DirectSound/dinput8.lib" )
+#pragma comment( lib, "AK/libraries/DirectSound/dsound.lib" ) 
+#pragma comment( lib, "AK/libraries/DirectSound/dxguid.lib" )  
 
 namespace FirulWwise
 {
