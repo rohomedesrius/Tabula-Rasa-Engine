e9557d3d3ff00aa497d00008c29ccf14b622202d
GottaCodeHarder <masterkonnen@gmail.com>
2018-12-21 14:32:29 +0100

    Audio Events for Component Audio

--- Tabula Rasa Engine/3D_Engine/ComponentAudio.cpp
+++ Tabula Rasa Engine/3D_Engine/ComponentAudio.cpp
@@ -15,6 +15,19 @@ bool ComponentAudio::Update(float dt)
 	return true;
 }
 
+void ComponentAudio::SetAudioCompType(COMP_TYPE type)
+{
+	audio_comp_type = type;
+}
+
+void ComponentAudio::CreateAudioEvent(const char * name)
+{
+	AudioEvent* new_event = new AudioEvent();
+	new_event->name = name;
+
+	audio_event = new_event;
+}
+
 bool ComponentAudio::Save(JSON_Object* component_obj)const
 {
 	return true;
--- Tabula Rasa Engine/3D_Engine/ComponentAudio.h
+++ Tabula Rasa Engine/3D_Engine/ComponentAudio.h
@@ -4,10 +4,27 @@
 #include "FirulWwise.h"
 #include "Component.h"
 
+#include <string>
+
 enum COMP_TYPE
 {
 	MUSIC,
 	LISTENER
+};
+
+class AudioEvent
+{
+public:
+
+	std::string name = "";
+	bool is_playing = false;
+
+	std::string state_group = "";
+	std::string state_a = "";
+	std::string state_b = "";
+	std::string* current_state = nullptr;
+
+	float transition = 0.0f;
 };
 
 class ComponentAudio : public Component
@@ -18,6 +35,9 @@ public:
 
 	bool Update(float dt);
 
+	void SetAudioCompType(COMP_TYPE type);
+	void CreateAudioEvent(const char* name);
+
 	//Scene Management
 	bool Save(JSON_Object* component_obj)const;
 	bool Load(const JSON_Object* component_obj);
@@ -26,6 +46,7 @@ private:
 
 	COMP_TYPE audio_comp_type = MUSIC;
 
+	AudioEvent* audio_event = nullptr;
 };
 
 #endif // __COMPONENT_AUDIO_H__
