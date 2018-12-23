3e01d57ba28e3c4a4c3f1ff3954228341c4f8359
GottaCodeHarder <masterkonnen@gmail.com>
2018-12-23 17:00:58 +0100

    Runtime execution Panel

--- Tabula Rasa Engine/3D_Engine/PanelAudio.cpp
+++ Tabula Rasa Engine/3D_Engine/PanelAudio.cpp
@@ -39,122 +39,131 @@ void PanelAudio::Draw()
 	
 	if (ImGui::CollapsingHeader("Create Emitters"))
 	{
-		ImGui::Combo("Scene Gameobjects", &current_emitter, go_names, i);
-		GameObject* temp = root->FindGOByName(go_names[current_emitter]);
-		
-		if (ImGui::Checkbox("Set as Emitter", &emitter))
-			listener = !listener;
+			ImGui::Combo("Scene Gameobjects", &current_emitter, go_names, i);
+			GameObject* temp = root->FindGOByName(go_names[current_emitter]);
 
-		ImGui::SameLine();
+			if (ImGui::Checkbox("Set as Emitter", &emitter))
+				listener = !listener;
 
-		if (ImGui::Checkbox("Set as Listener", &listener))
-			emitter = !emitter;
-
-		ImGui::Separator();
-
-		char* type_names[] = { "SFX", "MUSIC" };
-		static char event_name[41] = "";
-		static char state_group[41] = "";
-		static char state_a[41] = "";
-		static char state_b[41] = "";
-
-		static bool demo_music_event = true;
-		static bool demo_spacial_event = true;
-
-		static float transition = 8.0f;
-		
-		if (emitter)
-		{
-			ImGui::Combo("##ComboType", &type_current, type_names, IM_ARRAYSIZE(type_names));
 			ImGui::SameLine();
-			ImGui::Text("Audio Type");
 
-			switch (type_current)
+			if (ImGui::Checkbox("Set as Listener", &listener))
+				emitter = !emitter;
+
+			ImGui::Separator();
+
+			if (!App->IsRunTime())
 			{
-			case 0:
 
-				if (ImGui::Checkbox("Demo Spacial Sound Event", &demo_spacial_event))
+				char* type_names[] = { "SFX", "MUSIC" };
+				static char event_name[41] = "";
+				static char state_group[41] = "";
+				static char state_a[41] = "";
+				static char state_b[41] = "";
+
+				static bool demo_music_event = true;
+				static bool demo_spacial_event = true;
+
+				static float transition = 8.0f;
+
+				if (emitter)
 				{
-					if (!demo_spacial_event)
+					ImGui::Combo("##ComboType", &type_current, type_names, IM_ARRAYSIZE(type_names));
+					ImGui::SameLine();
+					ImGui::Text("Audio Type");
+
+					switch (type_current)
 					{
-						sprintf_s(event_name, IM_ARRAYSIZE(event_name), "");
+					case 0:
+
+						if (ImGui::Checkbox("Demo Spacial Sound Event", &demo_spacial_event))
+						{
+							if (!demo_spacial_event)
+							{
+								sprintf_s(event_name, IM_ARRAYSIZE(event_name), "");
+							}
+						}
+						if (demo_spacial_event)
+						{
+							sprintf_s(event_name, IM_ARRAYSIZE(event_name), "Play_Spatial_Sound");
+						}
+
+						ImGui::InputText("Event Name", event_name, 40);
+
+						break;
+					case 1:
+
+						if (ImGui::Checkbox("Demo Music Event", &demo_music_event))
+						{
+							if (!demo_music_event)
+							{
+								sprintf_s(event_name, IM_ARRAYSIZE(event_name), "");
+								sprintf_s(state_group, IM_ARRAYSIZE(state_group), "");
+								sprintf_s(state_a, IM_ARRAYSIZE(state_a), "");
+								sprintf_s(state_b, IM_ARRAYSIZE(state_b), "");
+							}
+						}
+						if (demo_music_event)
+						{
+							sprintf_s(event_name, IM_ARRAYSIZE(event_name), "Play_Music");
+							sprintf_s(state_group, IM_ARRAYSIZE(state_group), "Background");
+							sprintf_s(state_a, IM_ARRAYSIZE(state_a), "SongA");
+							sprintf_s(state_b, IM_ARRAYSIZE(state_b), "SongB");
+						}
+						ImGui::InputText("Event Name", event_name, 40);
+						ImGui::Spacing();
+
+						ImGui::Separator();
+						ImGui::Text("Event Configuration");
+						ImGui::SliderFloat("Duration", &transition, 3.0, 10.0, "%.f");
+						ImGui::Spacing();
+						ImGui::InputText("State group", state_group, 40);
+						ImGui::InputText("State 1", state_a, 40);
+						ImGui::InputText("State 2", state_b, 40);
+
+						break;
 					}
 				}
-				if (demo_spacial_event)
+				else if (listener)
 				{
-					sprintf_s(event_name, IM_ARRAYSIZE(event_name), "Play_Spatial_Sound");
+
 				}
 
-				ImGui::InputText("Event Name", event_name, 40);
+				ImGui::Separator();;
 
-				break;
-			case 1:
-
-				if (ImGui::Checkbox("Demo Music Event", &demo_music_event))
+				if (ImGui::Button("Set"))
 				{
-					if (!demo_music_event)
+					if (emitter)
 					{
-						sprintf_s(event_name, IM_ARRAYSIZE(event_name), "");
-						sprintf_s(state_group, IM_ARRAYSIZE(state_group), "");
-						sprintf_s(state_a, IM_ARRAYSIZE(state_a), "");
-						sprintf_s(state_b, IM_ARRAYSIZE(state_b), "");
+						AKEmitter* temp_emit = App->audio->CreateEmitter(go_names[current_emitter], temp, false, type_current);
+
+						if (temp_emit != nullptr)
+						{
+							ComponentAudio* temp_comp = AddComponentAudio(current_emitter);
+
+							temp_comp->CreateAudioEvent(event_name, transition, state_group, state_a, state_b);
+							temp_comp->SetEmitter(temp_emit);
+						}
+					}
+					else if (listener)
+					{
+						AKEmitter* temp_emit = App->audio->CreateEmitter(go_names[current_emitter], temp, true, NONE);
+
+						if (temp_emit != nullptr)
+						{
+							ComponentAudio* temp_comp = AddComponentAudio(current_emitter);
+
+							temp_comp->SetEmitter(temp_emit);
+						}
 					}
 				}
-				if (demo_music_event)
-				{
-					sprintf_s(event_name, IM_ARRAYSIZE(event_name), "Play_Music");
-					sprintf_s(state_group, IM_ARRAYSIZE(state_group), "Background");
-					sprintf_s(state_a, IM_ARRAYSIZE(state_a), "SongA");
-					sprintf_s(state_b, IM_ARRAYSIZE(state_b), "SongB");
-				}
-					ImGui::InputText("Event Name", event_name, 40);
-					ImGui::Spacing();
-
-					ImGui::Separator();
-					ImGui::Text("Event Configuration");
-					ImGui::SliderFloat("Duration", &transition, 3.0, 10.0, "%.f");
-					ImGui::Spacing();
-					ImGui::InputText("State group", state_group, 40);
-					ImGui::InputText("State 1", state_a, 40);
-					ImGui::InputText("State 2", state_b, 40);
-
-				break;
+				ImGui::SameLine();
+				ImGui::Text("<= Press to Create!");
 			}
-		}
-		else if (listener)
-		{
-
-		}
-
-		ImGui::Separator();;
-		if (ImGui::Button("Set"))
-		{
-			if (emitter)
+			else
 			{
-				AKEmitter* temp_emit = App->audio->CreateEmitter(go_names[current_emitter], temp, false, type_current);
-
-				if (temp_emit != nullptr)
-				{
-					ComponentAudio* temp_comp = AddComponentAudio(current_emitter);
-
-					temp_comp->CreateAudioEvent(event_name, transition, state_group, state_a, state_b);
-					temp_comp->SetEmitter(temp_emit);
-				}
+				ImGui::Text("You can't create Emitters on Execution!");
 			}
-			else if (listener)
-			{
-				AKEmitter* temp_emit = App->audio->CreateEmitter(go_names[current_emitter], temp, true, NONE);
-
-				if (temp_emit != nullptr)
-				{
-					ComponentAudio* temp_comp = AddComponentAudio(current_emitter);
-
-					temp_comp->SetEmitter(temp_emit);
-				}				
-			}
-		}
-		ImGui::SameLine();
-		ImGui::Text("<= Press to set READY!");
 	}
 	
 	if (ImGui::CollapsingHeader("Volume"))
@@ -186,7 +195,7 @@ void PanelAudio::Draw()
 
 		ImGui::Separator();
 
-		if (ImGui::SliderFloat("Spatial Volume", &spatial_volume, 0, 100))
+		if (ImGui::SliderFloat("SFX Volume", &spatial_volume, 0, 100))
 		{
 			std::list<AKEmitter*>::const_iterator it = App->audio->firul_emitters.begin();
 			while (it != App->audio->firul_emitters.end())
